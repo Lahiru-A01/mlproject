3933ebcaee5c8519946eb2464e191a2067fe96e5
Lahiru-A01 <lahiruavishka01@gmail.com>
2026-03-25 07:25:57 +0530

    Hyper para training

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
 
 from src.exception import CustomException
 
@@ -21,43 +23,24 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
+        for model_name, model in models.items():
+            para = param.get(model_name, {})
 
-            model.fit(X_train, y_train)  # Train model
+            if para:
+                gs = GridSearchCV(model, para, cv=3)
+                gs.fit(X_train, y_train)
+                model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
 
-            y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
-
-            train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
-
-            report[list(models.keys())[i]] = test_model_score
+            report[model_name] = test_model_score
 
         return report
 
     except Exception as e:
         raise CustomException(e, sys)
-
-from sklearn.metrics import r2_score
-
-def evaluate_models(X_train, y_train, X_test, y_test, models):
-    report = {}
-
-    for model_name, model in models.items():
-        # Train model
-        model.fit(X_train, y_train)
-
-        # Predict
-        y_pred = model.predict(X_test)
-
-        # Score
-        score = r2_score(y_test, y_pred)
-
-        report[model_name] = score
-
-    return report
